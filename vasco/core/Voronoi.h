0977330591632f44f27fe372a5f871c4857c2dc2
weichengdong <w@TANKK3>
2026-04-02 16:29:19 +0800

    输入文件处理逻辑小修改

--- vasco/core/Voronoi.h
+++ vasco/core/Voronoi.h
@@ -19,12 +19,12 @@ namespace vasco
     // 生成 Voronoi 单元
     // thresholdZ: z 高度阈值
     // visualize: 是否生成可视化 obj
-    // fileName: 基础文件名（不含扩展名）
+    // fileNameNoExt: 基础文件名（不含扩展名）
     void BuildVoronoiCells(const Eigen::MatrixXd& V,
                            const Eigen::MatrixXi& F,
                            double thresholdZ,
                            std::vector<VoronoiCell>& outCells,
                            std::vector<Eigen::Vector3d>& outBottomVertices,
                            bool visualize,
-                           const std::string& fileName);
+                           const std::string& fileNameNoExt);
 }
